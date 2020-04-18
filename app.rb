9384e58f5d8000ca9dc65782143aabe414faa79a
Beth Ruff <beth.ruff2@gmail.com>
2020-04-18 18:59:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @num = params[:number].to_i 
     @phrase = params[:phrase]
-    "#{@phrase}\n" * @num
+    "#{@phrase}" * @num
   end 
   
   get "/say/:word1/:word2/:word3/:word4/:word5" do 
