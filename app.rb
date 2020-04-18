c8fd6a4a91afc1ae59ef8a45ada44859fe47c4cf
Beth Ruff <beth.ruff2@gmail.com>
2020-04-18 19:02:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
-    "numb1.#{@operation}"
+    if @operation = 
   end 
 
 end 
