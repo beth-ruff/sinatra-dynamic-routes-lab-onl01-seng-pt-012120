4ebe34e523ee11c9b4595e07b20e24ab2ee4af08
Beth Ruff <beth.ruff2@gmail.com>
2020-04-18 18:56:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
   end
   
   get "/square/:number" do 
-    @number = params[:number] ** 2 
-    erb :squarenumber
+    @number = params[:number].to_i 
+    "#{@num ** 2}.to_s"  
   end 
   
   get "/say/:number/:phrase" do
