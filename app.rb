7556220ccc1eb6b77fb59e24d2731107222525f6
Beth Ruff <beth.ruff2@gmail.com>
2020-04-18 19:05:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,6 +31,14 @@ class App < Sinatra::Base
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
     if @operation == "add"
+      "#{@num1 + @num2}"
+    elsif @operation == "subtract"
+      "#{@num1 - @num2}"
+    elsif @operation == "multiply"
+      "#{@num1 * @num2}"
+    else @operation == "divide"
+      "#{@num1 / @num2}"
+    end 
   end 
 
 end 
